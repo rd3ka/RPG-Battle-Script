58c8be3f1428d7206a42ff18a994c637e73c7f5a
Raktim Deka <44165144+rdekajr@users.noreply.github.com>
2019-09-05 15:52:47 +0530

    Better View and Minor Bug Fixes

---
+++ game.py
@@ -0,0 +1,207 @@
+from .d3katool import *
+try:
+    import readchar
+except ModuleNotFoundError:
+    if  connection() == "InActive":
+        print("\n"+"Installation Of Package ReadChar Failed Due To No-Internet-Connection")
+        print('Try Installing It Manually Or Run This Program Again With An Active-Internet-Connection')
+        exit()
+    elif connection() == "Active":
+        print("ReadChar Package Not Found, PIP Will Now Install The Lastest Version Of "
+              + "ReadChar For You,Please Wait Till The Installation Completes")
+        install('readchar')
+        import readchar
+import random
+
+class FG:
+   BLACK = '\033[30m'
+   RED = '\033[31m'
+   GREEN = '\033[32m'
+   ORANGE = '\033[33m'
+   BLUE = '\033[34m'
+   PURPLE = '\033[35m'
+   CYAN = '\033[36m'
+   LGREY = '\033[37m'
+   DGREY = '\033[90m'
+   LRED = '\033[91m'
+   LGREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   LBLUE = '\033[94m'
+   PINK = '\033[95m'
+   LCYAN = '\033[96m'
+   BOLD = '\033[1m'
+   UNDERLINE = '\033[4m'
+   END = '\033[0m'
+
+
+class BG:
+   black = '\033[40m'
+   red = '\033[41m'
+   green = '\033[42m'
+   orange = '\033[43m'
+   blue = '\033[44m'
+   purple = '\033[45m'
+   cyan = '\033[46m'
+   lightgrey = '\033[47m'
+
+
+class Person:
+   def __init__(self, name, hp, atk, mp, df, magic, items):
+      self.name = name
+      self.max_hp = hp
+      self.hp = hp
+      self.atkl = atk - 10
+      self.atkh = atk + 10
+      self.max_mp = mp
+      self.mp = mp
+      self.df = df
+      self.magic = magic
+      self.items = items
+      self.action = [FG.YELLOW+"Press"+FG.BOLD+" [A] "+FG.END+FG.YELLOW+"To Attack"+FG.END, FG.YELLOW+"Press"+FG.BOLD+" [M] "+FG.END+FG.YELLOW+"To Cast Magic"+FG.END, FG.YELLOW+"Press"+FG.BOLD+" [I] "+FG.END+FG.YELLOW+"To Use Items"+FG.END,
+                     FG.YELLOW+"Press"+FG.BOLD+" [Q] "+FG.END+FG.YELLOW+"To Quit"+FG.END]
+
+   def get_name(self):
+       return self.name
+
+   def generate_attack_damage(self):
+      return random.randrange(self.atkl, self.atkh)
+
+   def take_damage(self, dmg):
+      self.hp = self.hp - dmg
+      if self.hp < 0:
+         self.hp = 0
+
+   def reduce_mp(self, cost):
+      self.mp = self.mp - cost
+
+   def heal(self, dmg):
+      if self.hp < self.max_hp:
+         self.hp = self.hp + dmg
+
+   def get_hp(self):
+      return self.hp
+
+   def get_mp(self):
+      return self.mp
+
+   def get_max_hp(self):
+      return self.max_hp
+
+   def get_max_mp(self):
+      return self.max_mp
+
+   def choose_action(self):
+      i = "*"
+      for index in self.action:
+         print(tabs(4)+FG.BOLD+str(i)+FG.END, str(index))
+
+   def choose_magic(self):
+      i = 1
+      for spl in self.magic:
+          if(len(spl.name) >= 6):
+              gap = 1
+          if(len(spl.name) < 5):
+              gap = 2
+          print(tabs(4)+str(i)+": "+FG.LCYAN+FG.BOLD+spl.name
+                + tabs(gap)+FG.END+"(cost:", str(spl.cost)+")")
+          i += 1
+      print("")
+
+   def choose_item(self):
+       i = 1
+       for itm in self.items:
+           if(len(itm["item"].name) >= 6):
+               gap = 2
+           if(len(itm["item"].name) < 5):
+               gap = 3
+           print(tabs(4)+str(i)+": "+FG.BLUE+FG.BOLD+itm["item"].name+FG.END+tabs(gap)+"(Type:", str(itm["item"].type)
+                 + ")"+tabs(2)+"(x" + str(itm["quantity"])+")")
+           i += 1
+       print("")
+
+   def choose_target(self, enemies):
+       choice = 0
+       i = 1
+       print(tabs(4)+FG.RED+FG.BOLD+"TARGET"+":"+FG.END+"\n")
+       for tgt in enemies:
+           if tgt.get_hp != 0:
+               print(tabs(4)+str(i)+": "+FG.PURPLE+FG.BOLD+tgt.name+FG.END)
+               i += 1
+       print("\n"+tabs(5)+FG.BOLD+"LOCK TARGET : "+"\n")
+       try:
+           buffer = readchar.readchar()
+           choice = int(buffer) - 1
+       except ValueError:
+           while buffer.isdigit() != True:
+               if buffer.isdigit() != True:
+                   buffer = readchar.readchar()
+               else:
+                   continue
+           while choice not in range(0, 4):
+                if choice not in range(0, 4):
+                    try:
+                        buffer = readchar.readchar()
+                        choice = int(buffer) - 1
+                    except ValueError:
+                        while buffer.isdigit() != True:
+                            if buffer.isdigit() != True:
+                                buffer = readchar.readchar()
+                            else:
+                                continue
+                else:
+                    continue
+       return choice
+
+   def get_player_stats(self):
+       hp_bar = ''
+       hp_tick = (self.hp/self.max_hp)*100/4
+       mp_bar = ''
+       mp_tick = (self.mp/self.max_mp)*100/10
+
+       while hp_tick > 0:
+           hp_bar += '█'
+           hp_tick -= 1
+       while len(hp_bar) < 25:
+           hp_bar += " "
+       while mp_tick > 0:
+           mp_bar += '█'
+           mp_tick -= 1
+       while len(mp_bar) < 10:
+           mp_bar += " "
+
+       if len(self.name) > 4 and len(self.name) < 13:
+           space = '\t\t'
+       if len(self.name) < 5:
+           space = '\t\t\t'
+       if len(self.name) > 13:
+           space = '\t'
+
+       print("\t"+FG.BOLD+FG.LGREEN+str(self.name)+FG.END+" : "+str(space)+str(self.hp)+"/"+str(self.max_hp)+spaces(4)+FG.LGREEN+str(hp_bar)+FG.END
+             + "\t"+str(self.mp)+"/"+str(self.max_mp)+"\t"+FG.LBLUE+str(mp_bar)+FG.END+"\n")
+
+   def get_enemy_stats(self):
+       hp_bar = space = ''
+       hp_tick = (self.hp/self.max_hp)*100/4
+       mp_bar = ''
+       mp_tick = (self.mp/self.max_mp)*100/10
+
+       while hp_tick > 0:
+          hp_bar += '█'
+          hp_tick -= 1
+       while len(hp_bar) < 25:
+          hp_bar += " "
+       while mp_tick > 0:
+       	  mp_bar += '█'
+          mp_tick -= 1
+       while len(mp_bar) < 10:
+       	  mp_bar += " "
+
+       if len(self.name) > 3 and len(self.name) < 13:
+          space = '\t\t'
+       if len(self.name) < 5:
+          space = '\t\t\t'
+       if len(self.name) > 13:
+          space = '\t'
+
+       print("\t"+FG.BOLD+FG.RED+str(self.name)+FG.END+" : "+str(space)+str(self.hp)+"/"+str(self.max_hp)+spaces(2)+FG.RED+str(hp_bar)+FG.END
+             + "\t"+str(self.mp)+"/"+str(self.max_mp)+"\t"+FG.PURPLE+str(mp_bar)+FG.END+"\n")
--- main.py
+++ main.py
@@ -22,53 +22,50 @@ except ModuleNotFoundError:
 clear()
 default_actions = ['a', 'm', 'i', 'q', 'A', 'M', 'I', 'Q']
 magic_choice = 0
-item_choice = 0
-#Object-Initialization
-Shuriken = item("Shuriken", "Weapon",
-                "A Ninja Blade Of ATK Type Dealing Damage", 80)
-Purple_Rose = item("Purple Rose", "Poison",
-                   "A Potion Of Poison Dealing Damage", 100)
-Kunai = item("Kunai", "Weapon",
-             "A Ninja Blade of ATK Type Dealing 45 Damage", 45)
-Ramen = item("Ramen", "Food", "SuperMeal To Increase H.P by 30%", 35)
-Elixer = item("Elixer", "Medical", "A Potion To Increase H.P by 50%", 50)
-item_list = [{"item": Shuriken, "quantity": 7},
+item_choice  = 0
+
+Shuriken 	= item("Shuriken", "Weapon","A Ninja Blade Of ATK Type Dealing Damage", 80)
+Purple_Rose = item("Purple Rose", "Poison","A Potion Of Poison Dealing Damage", 100)
+Kunai 		= item("Kunai", "Weapon","A Ninja Blade of ATK Type Dealing 45 Damage", 45)
+Ramen 		= item("Ramen", "Food", "SuperMeal To Increase H.P by 30%", 35)
+Elixer		= item("Elixer", "Medical", "A Potion To Increase H.P by 50%", 50)
+item_list   = [{"item": Shuriken, "quantity": 7},
              {"item": Purple_Rose, "quantity": 4},
              {"item": Kunai, "quantity": 5},
              {"item": Ramen, "quantity": 4},
              {"item": Elixer, "quantity": 2}]
 
-fire = spell("FIRE", 10, 100, "Black")
-thunder = spell("THUNDER", 10, 100, "Black")
+fire 	 = spell("FIRE", 10, 100, "Black")
+thunder  = spell("THUNDER", 10, 100, "Black")
 blizzard = spell("BLIZZARD", 10, 100, "Black")
-meteor = spell("METEOR", 20, 200, "Black")
-quake = spell("QUAKE", 14, 140, "Black")
-cure = spell("CURE", 18, 65, "White")
-cura = spell("CURA", 24, 80, "White")
+meteor   = spell("METEOR", 20, 200, "Black")
+quake    = spell("QUAKE", 14, 140, "Black")
+cure     = spell("CURE", 18, 65, "White")
+cura     = spell("CURA", 24, 80, "White")
 
 magic_list = [fire, thunder, blizzard, meteor, quake, cure, cura]
 
-#Name,Health-Points,Attack,Magic-Points,Defence,Magic-Spells,Items-To-Be-Used
 player1 = Person(input(FG.BOLD+FG.YELLOW+tabs(2)+"Enter Your Name Hero : "
-                       + FG.END+"\n\n"+tabs(4)).upper(), 6600, 660, 90, 125, magic_list, item_list)
-player2 = Person(input("\n\n"+FG.BOLD+FG.YELLOW+tabs(2)+"Enter The Name Of Your First Ally : "
-                       + FG.END+"\n\n"+tabs(4)).upper(), 2400, 240, 40, 75, magic_list, item_list)
-player3 = Person(input("\n\n"+FG.BOLD+FG.YELLOW+tabs(2)+"Enter The Name Of Your First Ally : "
-                       + FG.END+"\n\n"+tabs(4)).upper(), 2400, 240, 40, 75, magic_list, item_list)
+                + FG.END+"\n\n"+tabs(4)), 6600, 660, 90, 125, magic_list, item_list)
+player2 = Person('BatmanBecameDeadPool', 2400, 240, 40, 75, magic_list, item_list)
+player3 = Person('FirePhoenix', 2400, 240, 40, 75, magic_list, item_list)
 
 players = [player1, player2, player3]
+print()
 
-enemy1 = Person('RAKTIM', 17200, 610, 75, 125, magic_list, item_list)
-enemy2 = Person('sample1', 5500, 300, 45, 80, magic_list, item_list)
-enemy3 = Person('sample2', 5500, 300, 45, 80, magic_list, item_list)
+enemy1 = Person(input(FG.BOLD+FG.RED+tabs(2)+"Enter The Name Of Your Enemy : "
+               +FG.END+"\n\n"+tabs(4)), 17200, 610, 75, 125, magic_list, item_list)
+enemy2 = Person('StormTrooper', 5500, 300, 45, 80, magic_list, item_list)
+enemy3 = Person('DarkAlexia', 5500, 300, 45, 80, magic_list, item_list)
 
 enemies = [enemy1, enemy2, enemy3]
 running = True
 clear()
-#GAME-LOOP
+
 while running:
-    print("\t"+FG.PINK+FG.BOLD+FG.UNDERLINE+"NAME"+FG.END+tabs(3)+FG.BOLD+FG.PINK+FG.UNDERLINE
-          + "H.P"+tabs(4)+FG.END+FG.PINK+FG.BOLD+FG.UNDERLINE + "\t"+"M.P"+FG.END+"\n")
+    print("\t"+FG.BOLD+FG.UNDERLINE+"NAME"+FG.END+tabs(3)+FG.BOLD+FG.UNDERLINE
+          + "H.P"+tabs(4)+FG.END+FG.BOLD+FG.UNDERLINE + "\t"+"M.P"+FG.END)
+    print(tabs(1)+'-'*82 + "\n")
     for player in players:
         player.get_player_stats()
 
@@ -76,11 +73,14 @@ while running:
 
     for enemy in enemies:
         enemy.get_enemy_stats()
-#HERO
+
+    print(tabs(1)+'-'*82 + "\n")
+
+	#H E R O
     for player in players:
         p_name = player.get_name()
-        print("\n"+FG.BOLD+tabs(4)+spaces(5)+FG.UNDERLINE
-              + p_name.upper()+FG.END+spaces(5)+tabs(4)+"\n")
+        print("\n"+FG.BOLD+tabs(6)+spaces(3)+FG.UNDERLINE
+              + p_name+FG.END+spaces(5)+tabs(4)+"\n")
         player.choose_action()
         print("\n\t"+FG.BOLD+tabs(2)+"USE : "+FG.END)
         player_choice = readchar.readchar()
@@ -97,7 +97,7 @@ while running:
             if decision == 'Y' or decision == 'y':
                 clear()
                 print("\n"+FG.BOLD+" GAME OVER! "*10+"\n")
-                sleep(3)
+                sleep(0)
                 exit()
 
             elif decision == 'N' or decision == 'n':
@@ -218,7 +218,7 @@ while running:
 
     clear()
 
-#ENEMY
+    #E N E M Y
     for player in players:
         player.get_player_stats()
 
@@ -227,12 +227,15 @@ while running:
     for enemy in enemies:
         enemy.get_enemy_stats()
 
+    print(tabs(1)+'-'*82 + "\n")
+
     for enemy in enemies:
         print("\n"+FG.BOLD+tabs(4)+spaces(4)+FG.UNDERLINE
-              + str(enemy.name).upper()+FG.END+spaces(4)+tabs(4)+"\n")
+              + str(enemy.name)+FG.END+spaces(4)+tabs(4)+"\n")
 
         enemy.choose_action()
-        enemy_choice = random.randrange(1, 4)
+        print("")
+        enemy_choice = random.randrange(0,3)
 
         if enemy_choice == 1:
             enemy_dmg = enemy.generate_attack_damage()
@@ -240,31 +243,29 @@ while running:
             players[target].take_damage(enemy_dmg)
             print("\n"+tabs(3)+FG.PURPLE+FG.BOLD+str(enemy.name).upper()+" Attacked "
                   + str(players[target].name)+" For "+str(enemy_dmg)+" Damage Points"+FG.END+"\n")
-            sleep(2)
+            sleep(3)
 
         elif enemy_choice == 2:
             enemy.choose_magic()
             enemy_spell_choice = random.randrange(0, len(magic_list))
-            #print("magic",enemy_spell_choice)
             spell = enemy.magic[enemy_spell_choice]
             enemy_magical_dmg = spell.generate_spell_damage()
-            target = random.randrange(0,4)
+            target = random.randrange(0,3)
 
             if spell.type == 'Black':
                 players[target].take_damage(enemy_magical_dmg)
                 print(tabs(3)+FG.BLUE+FG.BOLD+str(spell.name)+" Attacked "
-                      + str(players[target].name)+" For "+str(enemy_magical_dmg)+" Damage Points"+FG.END)
-                sleep(1.8)
+                      + str(players[target].name)+" For "+str(enemy_magical_dmg)+" Damage Points"+FG.END+"\n")
+                sleep(4)
             if spell.type == 'White':
                 enemy.heal(enemy_magical_dmg)
                 print(tabs(3)+FG.BLUE+FG.BOLD+str(spell.name)
-                      + " Healed For "+str(enemy_magical_dmg)+" H.P "+FG.END)
-                sleep(1.8)
+                      + " Healed For "+str(enemy_magical_dmg)+" H.P "+FG.END+"\n")
+                sleep(4)
 
                 enemy.reduce_mp(spell.cost)
 
             if spell.cost > enemy.get_mp():
-                #print(tabs(3)+FG.RED+" You Do Not Have Enough M.P "+FG.END)
                 continue
         elif enemy_choice == 3:
             enemy.choose_item()
@@ -272,23 +273,19 @@ while running:
             item = enemy.items[enemy_item_choice]["item"]
             target = random.randrange(0, 3)
             if enemy.items[enemy_item_choice]["quantity"] == 0:
-                #print(FG.BOLD+FG.LGREY+"\nNoNe Left"+FG.END)
                 continue
             enemy.items[enemy_item_choice]["quantity"] -= 1
             item_dmg = item.generate_item_dmg()
-            print(item_dmg)
 
             if item.type == 'Weapon' or item.type == 'Poison':
-                print(target)
                 players[target].take_damage(item_dmg)
                 print(tabs(3)+FG.BLUE+FG.BOLD+str(item.name)
-                      + " Attacked "+str(players[target].name)+" For "+str(item_dmg)+" Damage Point "+FG.END)
-                sleep(1.8)
+                      + " Attacked "+str(players[target].name)+" For "+str(item_dmg)+" Damage Point "+FG.END+"\n")
+                sleep(3)
 
             if item.type == 'Medical' or item.type == 'Food':
                 enemy.heal(item_dmg)
                 print(tabs(3)+FG.BLUE+FG.BOLD+str(item.name)
-                      + " Healed For "+str(item_dmg)+" H.P "+FG.END)
+                      + " Healed For "+str(item_dmg)+" H.P "+FG.END+"\n")
                 sleep(1.8)
     clear()
-
